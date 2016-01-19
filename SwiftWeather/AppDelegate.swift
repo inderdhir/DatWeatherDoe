0a9c66588757f4d27912c3eb4b4cf66b5148f375
Inder Dhir <inderdhir@hotmail.com>
2016-01-19 15:14:21 -0500

    Initial Commit

---
+++ SwiftWeather/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftWeather
+//
+//  Created by Inder Dhir on 1/19/16.
+//  Copyright © 2016 Inder Dhir. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
