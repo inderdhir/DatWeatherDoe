e394a2cbf763f6b70d0668933803419467b6c9e1
Inder Dhir <inderdhir03@gmail.com>
2023-01-21 22:22:24 -0500

    Fix weather source text not being loaded properly

--- DatWeatherDoe/UI/Configure/ConfigureViewModel.swift
+++ DatWeatherDoe/UI/Configure/ConfigureViewModel.swift
@@ -11,11 +11,13 @@ import Foundation
 import LaunchAtLogin
 
 final class ConfigureViewModel: ObservableObject {
-    
+
     @Published var temperateUnit: TemperatureUnit {
-        didSet { configManager.temperatureUnit = temperateUnit.rawValue }
+        didSet {
+            configManager.temperatureUnit = temperateUnit.rawValue
+        }
     }
-    
+
     @Published var weatherSource: WeatherSource {
         didSet {
             configManager.weatherSource = weatherSource.rawValue
@@ -26,41 +28,53 @@ final class ConfigureViewModel: ObservableObject {
     @Published private(set) var weatherSourceTextFieldDisabled = false
     @Published private(set) var weatherSourcePlaceholder = ""
     @Published var weatherSourceText = "" {
-        didSet { configManager.weatherSourceText = weatherSourceText }
+        didSet {
+            configManager.weatherSourceText = weatherSourceText
+        }
     }
-    
+
     @Published var refreshInterval: RefreshInterval {
-        didSet { configManager.refreshInterval = refreshInterval.rawValue }
+        didSet {
+            configManager.refreshInterval = refreshInterval.rawValue
+        }
     }
-    
+
     @Published var isShowingWeatherIcon: Bool {
-        didSet { configManager.isShowingWeatherIcon = isShowingWeatherIcon }
+        didSet {
+            configManager.isShowingWeatherIcon = isShowingWeatherIcon
+        }
     }
-    
+
     @Published var isShowingHumidity: Bool {
-        didSet { configManager.isShowingHumidity = isShowingHumidity }
+        didSet {
+            configManager.isShowingHumidity = isShowingHumidity
+        }
     }
-    
+
     @Published var isRoundingOffData: Bool {
-        didSet { configManager.isRoundingOffData = isRoundingOffData }
+        didSet {
+            configManager.isRoundingOffData = isRoundingOffData
+        }
     }
-    
+
     @Published var isWeatherConditionAsTextEnabled: Bool {
-        didSet { configManager.isWeatherConditionAsTextEnabled = isWeatherConditionAsTextEnabled }
+        didSet {
+            configManager.isWeatherConditionAsTextEnabled = isWeatherConditionAsTextEnabled
+        }
     }
-    
+
     @Published var launchAtLogin = LaunchAtLogin.observable
-    
+
     private let configManager: ConfigManagerType
     private weak var popoverManager: PopoverManager?
-    
+
     init(configManager: ConfigManagerType, popoverManager: PopoverManager?) {
         self.configManager = configManager
         self.popoverManager = popoverManager
-        
+
         temperateUnit = TemperatureUnit(rawValue: configManager.temperatureUnit)!
         weatherSource = WeatherSource(rawValue: configManager.weatherSource)!
-       
+
         switch configManager.refreshInterval {
         case 300: refreshInterval = .fiveMinutes
         case 900: refreshInterval = .fifteenMinutes
@@ -68,29 +82,33 @@ final class ConfigureViewModel: ObservableObject {
         case 3600: refreshInterval = .sixtyMinutes
         default: refreshInterval = .oneMinute
         }
-        
+
         isShowingWeatherIcon = configManager.isShowingWeatherIcon
         isShowingHumidity = configManager.isShowingHumidity
         isRoundingOffData = configManager.isRoundingOffData
         isWeatherConditionAsTextEnabled = configManager.isWeatherConditionAsTextEnabled
-        
+
         updateWeatherSource()
     }
-    
+
     func saveAndCloseConfig() {
         saveConfig()
         popoverManager?.togglePopover(nil)
     }
-    
+
     private func updateWeatherSource() {
         weatherSourceTextHint = weatherSource.textHint
         weatherSourceTextFieldDisabled = weatherSource == .location
+
         if weatherSource == .location {
             weatherSourceText = ""
+        } else if let sourceText = configManager.weatherSourceText {
+            weatherSourceText = sourceText
         }
+
         weatherSourcePlaceholder = weatherSource.placeholder
     }
-    
+
     private func saveConfig() {
         let configCommitter = ConfigurationCommitter(configManager: configManager)
         configCommitter.setWeatherSource(weatherSource, sourceText: weatherSourceText)
