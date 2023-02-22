439def15777822cda641e86a4d4bdd22231b370a
preckrasno <dev@nazh.me>
2023-02-22 12:15:34 +0200

    PR-113 Remove redundant functions in city tests

--- DatWeatherDoeTests/API/Repository/City/CityWeatherURLBuilderTests.swift
+++ DatWeatherDoeTests/API/Repository/City/CityWeatherURLBuilderTests.swift
@@ -11,14 +11,6 @@ import XCTest
 
 class CityWeatherURLBuilderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testBuild() {
         XCTAssertEqual(
             try? CityWeatherURLBuilder(appId: "123456", city: "Kyiv,ua")
