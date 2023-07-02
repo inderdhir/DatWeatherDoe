94d822c7f2a1592c5747df5d6fce63b295ee0ffc
Inder Dhir <inderdhir03@gmail.com>
2023-07-02 12:40:35 -0400

    Add accessibliity description for images

--- DatWeatherDoe/UI/Decorator/Condition/Mapping/WeatherConditionImageMapper.swift
+++ DatWeatherDoe/UI/Decorator/Condition/Mapping/WeatherConditionImageMapper.swift
@@ -12,41 +12,51 @@ final class WeatherConditionImageMapper {
     
     func map(_ condition: WeatherCondition) -> NSImage? {
         let symbolName: String
+        let accessibilityDescription: String
         
         switch condition {
         case .cloudy:
             symbolName = "cloud"
+            accessibilityDescription = "Cloudy"
             
         case .partlyCloudy:
             symbolName = "cloud.sun"
+            accessibilityDescription = "Partly Cloudy"
         case .partlyCloudyNight:
             symbolName = "cloud.moon"
+            accessibilityDescription = "Partly Cloudy"
             
         case .sunny:
             symbolName = "sun.max"
+            accessibilityDescription = "Sunny"
         case .clearNight:
             symbolName = "moon"
+            accessibilityDescription = "Clear"
             
         case let .smoky(smokyWeatherCondition):
             return SmokyWeatherConditionImageMapper().map(smokyWeatherCondition)
             
         case .snow:
             symbolName = "cloud.snow"
+            accessibilityDescription = "Snow"
             
         case .lightRain, .heavyRain, .freezingRain:
             symbolName = "cloud.rain"
+            accessibilityDescription = "Rainy"
         
         case .partlyCloudyRain:
             symbolName = "cloud.sun.rain"
+            accessibilityDescription = "Partly cloudy with rain"
 
         case .thunderstorm:
             symbolName = "cloud.bolt.rain"
+            accessibilityDescription = "Thunderstorm"
         }
         
         let config = NSImage.SymbolConfiguration(textStyle: .title2, scale: .medium)
         return NSImage(
             systemSymbolName: symbolName,
-            accessibilityDescription: nil
+            accessibilityDescription: accessibilityDescription
         )?.withSymbolConfiguration(config)
     }
 }
@@ -54,16 +64,23 @@ final class WeatherConditionImageMapper {
 private class SmokyWeatherConditionImageMapper {
     func map(_ condition: SmokyWeatherCondition) -> NSImage? {
         let symbolName: String
+        let accessibilityDescription: String
         
         switch condition {
         case .tornado:
             symbolName = "tornado"
+            accessibilityDescription = "Tornado"
         case .squall:
             symbolName = "wind"
+            accessibilityDescription = "Squall"
         default:
             symbolName = "cloud.fog"
+            accessibilityDescription = "Cloudy with Fog"
         }
         
-        return NSImage(systemSymbolName: symbolName, accessibilityDescription: nil)
+        return NSImage(
+            systemSymbolName: symbolName,
+            accessibilityDescription: accessibilityDescription
+        )
     }
 }
--- DatWeatherDoe/UI/Menu Bar/DropdownIconMapper.swift
+++ DatWeatherDoe/UI/Menu Bar/DropdownIconMapper.swift
@@ -9,24 +9,29 @@
 import Cocoa
 
 final class DropdownIconMapper {
-
+    
     func map(_ icon: DropdownIcons) -> NSImage? {
         let symbolName: String
-
+        let accessibilityDescription: String
+        
         switch icon {
         case .location:
             symbolName = "mappin"
-        case .sun:
-            symbolName = "sun.max"
+            accessibilityDescription = "Location"
         case .thermometer:
             symbolName = "thermometer"
+            accessibilityDescription = "Temperature"
+        case .sun:
+            symbolName = "sun.max"
+            accessibilityDescription = "Sunrise and Sunset"
         case .wind:
             symbolName = "wind"
+            accessibilityDescription = "Wind data"
         }
-
+        
         let config = NSImage.SymbolConfiguration(textStyle: .title2, scale: .medium)
-        return NSImage(systemSymbolName: symbolName, accessibilityDescription: nil)?.withSymbolConfiguration(
-            config
-        )
+        return NSImage(
+            systemSymbolName: symbolName,
+            accessibilityDescription: accessibilityDescription)?.withSymbolConfiguration(config)
     }
 }
