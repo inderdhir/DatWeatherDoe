4a2c2d5dce5332639377da34fc946107e12675f8
Inder Dhir <inderdhir03@gmail.com>
2024-10-18 20:35:54 -0400

    Updatae language mode to Swift 6

--- DatWeatherDoe/UI/Decorator/Condition/WeatherConditionBuilder.swift
+++ DatWeatherDoe/UI/Decorator/Condition/WeatherConditionBuilder.swift
@@ -22,29 +22,29 @@ final class WeatherConditionBuilder: WeatherConditionBuilderType {
     func build() -> WeatherCondition {
         switch response.weatherConditionCode {
         case 1006, 1009:
-            return .cloudy
+            .cloudy
         case 1003:
-            return response.isDay ? .partlyCloudy : .partlyCloudyNight
+            response.isDay ? .partlyCloudy : .partlyCloudyNight
         case 1000:
-            return response.isDay ? .sunny : .clearNight
+            response.isDay ? .sunny : .clearNight
         case 1030:
-            return .mist
+            .mist
         case 1135, 1147:
-            return .fog
+            .fog
         case 1066,
              1114, 1117,
              1210, 1213, 1216, 1219, 1222, 1225, 1237, 1249, 1252, 1255, 1258, 1261, 1264, 1279, 1282:
-            return .snow
+            .snow
         case 1192, 1195, 1243, 1246, 1276:
-            return .heavyRain
+            .heavyRain
         case 1069, 1072, 1168, 1171, 1198, 1201, 1204, 1207:
-            return .freezingRain
+            .freezingRain
         case 1063, 1150, 1153, 1180, 1183, 1186, 1189, 1240:
-            return response.isDay ? .partlyCloudyRain : .lightRain
+            response.isDay ? .partlyCloudyRain : .lightRain
         case 1087, 1273:
-            return .thunderstorm
+            .thunderstorm
         default:
-            return WeatherCondition.getFallback(isDay: response.isDay)
+            WeatherCondition.getFallback(isDay: response.isDay)
         }
     }
 }
